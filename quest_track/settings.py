06b45baa26fdbc218e60e8593f2995627771beca
Mithoonns2001 <mithoonns@gmail.com>
2023-07-13 11:16:42 +0530

    updated

--- quest_track/settings.py
+++ quest_track/settings.py
@@ -74,6 +74,18 @@ WSGI_APPLICATION = 'quest_track.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'mithoon',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',  
+#         'PORT': '3306',  
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
